acff74d56886ceb00cc09ae6968633bc79e04ea7
Amrik Verma <amrikverma23@gmail.com>
2024-02-03 14:25:38 -0800

    Authentication Completed
    Sign Up, Sign In, Sign Out buttons and functionality finished

--- react-firebase-auth/src/App.js
+++ react-firebase-auth/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import AuthDetails from './components/auth/AuthDetails';
 
 function App() {
   return (
     <div className="App"> 
-      <SignIn />
+      <SignIn/>
+      <SignUp/>
+      <AuthDetails/>
     </div>
   );
 }
