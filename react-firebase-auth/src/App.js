5a74cc42486ac152386b2effd79a2d73179bc943
KaptainKermit <kupresaninmarko2@gmail.com>
2024-02-03 14:37:10 -0800

    CHATBOT CLUTCH

--- react-firebase-auth/src/App.js
+++ react-firebase-auth/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn';
+import ChatBot from 'react-simple-chatbot';
+ 
+const steps = [
+    {
+        id: '0',
+        message: 'Hey Geek!',
+        end: true
+    }
+];
 
 function App() {
   return (
     <div className="App"> 
       <SignIn />
+
     </div>
   );
 }
