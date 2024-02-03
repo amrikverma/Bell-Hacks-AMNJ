29bad61ee023c410410cfdb15532a867f834e29e
nkfire11 <41340519+nkfire11@users.noreply.github.com>
2024-02-03 15:58:13 -0800

    NavBar stuff

--- react-firebase-auth/src/App.js
+++ react-firebase-auth/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './components/auth/SignIn';
+import NavBar from './NavBar';
+
 
 function App() {
   return (
     <div className="App"> 
+      <NavBar />
       <SignIn />
     </div>
   );
---
+++ react-firebase-auth/src/NavBar.js
@@ -0,0 +1,15 @@
+export default function NavBar(){
+    return (
+        <nav className="nav">
+            <a href="/" className="site-title">Golden Yrs</a>
+            <ul>
+                <li>
+                    <a href="/pricing">Log in</a>
+                    
+                </li>
+            </ul>
+        </nav> 
+    )
+}
+    
+
