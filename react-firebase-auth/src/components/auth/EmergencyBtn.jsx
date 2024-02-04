d2887c554906dcbb3ad6d47356845a958045d239
KaptainKermit <kupresaninmarko2@gmail.com>
2024-02-03 16:00:09 -0800

    fixed email backedn

--- react-firebase-auth/src/components/auth/EmergencyBtn.jsx
+++ react-firebase-auth/src/components/auth/EmergencyBtn.jsx
@@ -4,19 +4,31 @@ import emailjs from '@emailjs/browser'
 const EmgBtn = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    // const [patient_name, setPatientName] = useState("");
+    // const [emergency_contact_name, setEmergencyContactName] = useState("");
+    var templateParams = {
+        emergency_contact_email: "test@test.test",
+        emergency_contact_name: "young person name",
+        patient_name: "old person name"
+    }
 
     const handleClick = (e) => {
+        // console.log(e.target)
         e.preventDefault();
-        emailjs.sendForm('service_8rvo9b9', 'template_k8qolbg', e.target, 'c_2qCBvKJ3ocqXu5x')
+        emailjs.send('service_8rvo9b9', 'template_k8qolbg', templateParams, 'c_2qCBvKJ3ocqXu5x')
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert(response.status)
+            })
     };
 
     return (
-        <div id="emergencybutton" style={{ position: "fixed", bottom: "10px", left: "10px", zIndex: "1000"}}>
+        <div style={{ position: "fixed", bottom: "10px", left: "10px", zIndex: "1000", cursor: "help" }}>
             <button onClick={handleClick} style={{ padding: "0", border: "none", background: "none" }}>
                 <img 
                     src="https://th.bing.com/th/id/R.5e6d8788a6cdc41aae6d7a4b131ec2f8?rik=TPPufOG2LggJxA&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fdcr%2f6yG%2fdcr6yGrKi.jpg&ehk=ze5MszvAz9lfvLBE4oIcWjNfk7GHGz2YkPJGsMOcyPY%3d&risl=&pid=ImgRaw&r=0" 
                     alt="Emergency Button"
-                    style={{ width: "30px", height: "30px" }} // Adjust the width and height as needed
+                    style={{ width: "30px", height: "30px" }} 
                 />
             </button>
         </div>
