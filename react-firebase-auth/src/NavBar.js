b101836fb9ba43eff0c8cb6a0cb3efe05662506d
Justin Shen <140134587+Justinshen23@users.noreply.github.com>
2024-02-03 18:16:34 -0800

    changes hooray

--- react-firebase-auth/src/NavBar.js
+++ react-firebase-auth/src/NavBar.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export default function NavBar(){
     return (
         <nav className="nav">
-        <a href="/Home" className="site-title">Golden Yrs</a>
+        <a href="/" className="site-title">Golden Yrs</a>
             <ul>
                 <li>
                     <a href="/signup"> Sign Up</a>
