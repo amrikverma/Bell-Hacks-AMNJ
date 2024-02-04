46245e22fa74b7372515fb23f3ea3f3efecfd0b6
KaptainKermit <kupresaninmarko2@gmail.com>
2024-02-03 18:55:49 -0800

    final css :smile:

--- react-firebase-auth/src/NavBar.jsx
+++ react-firebase-auth/src/NavBar.jsx
@@ -3,19 +3,19 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 export default function NavBar(){
     return (
         <nav className="nav">
-        <a href="/" className="site-title">Golden Yrs</a>
+        <a href="/" style={{fontSize: "30px", paddingTop: "10px"}} className="site-title">Golden Yrs</a>
             <ul>
                 <li>
-                    <a href="/signup"> Sign Up</a>
+                    <a style={{fontSize: "20px", paddingTop: "10px"}} href="/signup"> Sign Up</a>
                 </li>
                 <li>
                     
-                    <a href="signin">Log in</a>
+                    <a style={{fontSize: "20px", paddingTop: "10px"}} href="signin">Log in</a>
                     
                 </li>
                 <li>
                     
-                    <a href="/logout">Log out</a>
+                    <a style={{fontSize: "20px", paddingTop: "10px"}} href="/logout">Log out</a>
                     
                 </li>
             </ul>
--- react-firebase-auth/src/components/auth/EmergencyBtn.jsx
+++ react-firebase-auth/src/components/auth/EmergencyBtn.jsx
@@ -11,20 +11,19 @@ const EmgBtn = () => {
         emergency_contact_name: "young person name",
         patient_name: "old person name"
     }
-
     const handleClick = (e) => {
         // console.log(e.target)
         e.preventDefault();
         emailjs.send('service_8rvo9b9', 'template_k8qolbg', templateParams, 'c_2qCBvKJ3ocqXu5x')
             .then(function(response) {
                 console.log('SUCCESS!', response.status, response.text);
-                alert(response.status)
+                alert("Do not fret, email was sent to " + email)
             })
     };
 
     return (
         <div style={{ position: "fixed", bottom: "10px", left: "10px", zIndex: "1000", cursor: "help" }}>
-            <button onClick={handleClick} style={{ padding: "0", border: "none", background: "none" }}>
+            <button onClick={handleClick} onRe style={{ padding: "0", border: "none", background: "none" }}>
                 <img 
                     src="https://th.bing.com/th/id/R.5e6d8788a6cdc41aae6d7a4b131ec2f8?rik=TPPufOG2LggJxA&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fdcr%2f6yG%2fdcr6yGrKi.jpg&ehk=ze5MszvAz9lfvLBE4oIcWjNfk7GHGz2YkPJGsMOcyPY%3d&risl=&pid=ImgRaw&r=0" 
                     alt="Emergency Button"
